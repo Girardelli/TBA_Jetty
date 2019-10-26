46ab56725c2be344b44a193b95e5e6ddb9e44e8a
Girardelli <girardelli65@gmail.com>
2019-10-26 18:22:32 +0200

    Update TbaWebSocketAdapter.java

--- be/tba/websockets/TbaWebSocketAdapter.java
+++ be/tba/websockets/TbaWebSocketAdapter.java
@@ -72,10 +72,13 @@ public class TbaWebSocketAdapter extends WebSocketAdapter
 
    public static void broadcast(WebSocketData data)
    {
+      System.out.println("broadcast: ");
       if (data.operation == WebSocketData.CALL_ANSWERED)
       {
          // this session ID shall be used by the canvas JSP to open the call update window automatically.
          data.answeredBySession = IntertelCallManager.getInstance().getSessionIdForPhoneId(data.answeredByPhoneId);
+         System.out.println("data.answeredBySession=" + data.answeredBySession);
+         
       }
       
       Collection<WebSession> activeSessions = SessionManager.getInstance().getActiveWebSockets();
@@ -86,6 +89,7 @@ public class TbaWebSocketAdapter extends WebSocketAdapter
          {
             try
             {
+               System.out.println("send websocket event:");
                session.getWsSession().getRemote().sendString(sGson.toJson(data, WebSocketData.class));
             } catch (IOException e)
             {
