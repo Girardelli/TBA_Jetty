94126721112046b081e1eb4139f0af5c62846096
Girardelli <girardelli65@gmail.com>
2019-11-09 21:40:05 +0100

    clean up

--- be/tba/servlets/helper/IntertelCallManager.java
+++ be/tba/servlets/helper/IntertelCallManager.java
@@ -118,19 +118,19 @@ public class IntertelCallManager
       // that has NOT this ID
       // CallingNr: tba number
       // calledNr: number to who the call is transfered
-int y = 1;
+//int y = 1;
       for (Iterator<IntertelCallData> i = mCallMap.values().iterator(); i.hasNext();)
       {
          IntertelCallData call = i.next();
-         System.out.println("getTransferCall_CallParkBugs " + y++);
-         System.out.println(transferedCall);
-         System.out.println(call);
+//         System.out.println("getTransferCall_CallParkBugs " + y++);
+//         System.out.println(transferedCall);
+//         System.out.println(call);
          if (call != transferedCall && call.isIncoming && call.tsEnd == 0 && transferedCall.callingNr.equals(call.answeredBy))
          {
             return call;
          }
       }
-      System.out.println("getTransferCall_CallParkBugs returns null");
+//      System.out.println("getTransferCall_CallParkBugs returns null");
       return null;
    }
    
